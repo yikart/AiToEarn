bb386782d744b37ed7aa78246236af8f446b44f0
gaoSir <y17600917357@163.com>
2025-03-04 16:59:23 +0800

    fix: 视频号api登录状态失效捕获

--- electron/main/plat/platforms/wxSph/index.ts
+++ electron/main/plat/platforms/wxSph/index.ts
@@ -182,7 +182,11 @@ export class WxSph extends PlatformBase {
       cookie: params.cookie!,
     });
     return {
-      status: locationRes.data.errCode === 300334 || locationRes.data.errCode === 300333 ? 401 : locationRes.status,
+      status:
+        locationRes.data.errCode === 300334 ||
+        locationRes.data.errCode === 300333
+          ? 401
+          : locationRes.status,
       data: locationRes?.data?.data?.list?.map((v) => {
         return {
           name: v.name,
