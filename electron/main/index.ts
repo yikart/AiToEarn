0fb01f2df6c18ab32abc5fdec3b3e9deaf9697ea
牛文政 <861796052@qq.com>
2025-03-08 21:33:54 +0800

    feat: 关闭跨域

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -59,6 +59,7 @@ async function createWindow() {
     webPreferences: {
       preload,
       webviewTag: true,
+      webSecurity: false,
       nodeIntegration: false,
       contextIsolation: true,
     },
