8bb903739056f8191a616c8f2d0bb1626d66e3db
牛文政 <861796052@qq.com>
2025-03-10 15:08:14 +0800

    feat: banner

--- src/views/test/video/index.tsx
+++ src/views/test/video/index.tsx
@@ -5,40 +5,18 @@
  * @LastEditors: nevin
  * @Description: 视频测试
  */
-import VideoChoose from '@/components/Choose/VideoChoose';
-import { ipcGetVideoCover } from '@/icp/tools';
-import Update from '@/components/update';
+import { operateApi } from '@/api/operate';
 import { Button } from 'antd';
 
 export default function Page() {
-  async function onChoose(e: any) {
-    console.log('---- ', e.videoPath);
-
-    const res = await ipcGetVideoCover(e.videoPath);
-
-    console.log('---- ', res);
-  }
-
-  async function setChromPath() {
-    const res: string = await window.ipcRenderer.invoke(
-      'ICP_SET_CHROMIUM_PATH',
-    );
-    console.log('---- ', res, '----');
-  }
-
-  async function getChromPath() {
-    const res = await window.ipcRenderer.invoke('ICP_APP_GET_INFO');
-    console.log('---- ', res, '----');
+  async function getBanner() {
+    const res = await operateApi.getBannerList();
+    console.log('----banner ', res);
   }
 
   return (
     <div>
-      <h1>视频测试</h1>
-      <VideoChoose onChoose={onChoose} />
-      1111
-      <Update />
-      <Button onClick={getChromPath}>获取 Chromium 路径</Button>
-      <Button onClick={setChromPath}>设置 Chromium 路径</Button>
+      <Button onClick={getBanner}>获取banner</Button>
     </div>
   );
 }
