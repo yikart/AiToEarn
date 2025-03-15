cb323bafe5fbfdd222a3688e2ca1a03e8b6cbc45
gaoSir <y17600917357@163.com>
2025-03-15 18:47:26 +0800

    feat: 自定义顶部标题栏完成

--- electron/main/service.ts
+++ electron/main/service.ts
@@ -7,13 +7,16 @@
 import { app } from 'electron';
 import { Injectable } from './core/decorators';
 import { getChromiumPath, setChromiumPath } from '../util/chromium';
+import os from 'os';
 
 @Injectable()
 export class AppService {
   getAppInfo() {
+    const platform = os.platform();
     return {
       version: app.getVersion(),
       chromiumPath: getChromiumPath(),
+      platform: platform,
     };
   }
 
