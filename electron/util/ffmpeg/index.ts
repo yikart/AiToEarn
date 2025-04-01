2c26a6ab1ada76660ab9b397408f5ab1a1810f1e
牛文政 <861796052@qq.com>
2025-04-01 15:20:35 +0800

    feat: 更换包

--- electron/util/ffmpeg/index.ts
+++ electron/util/ffmpeg/index.ts
@@ -5,32 +5,14 @@
  * @LastEditors: nevin
  * @Description: ffmpeg工具
  */
-import ffmpeg from 'fluent-ffmpeg'; // 一个封装了 ffmpeg API 的库，当然可以选择不安装，直接使用字符串拼接的方式调用
-import os from 'os';
-import { app } from 'electron';
-import path from 'path';
-import { fileURLToPath } from 'node:url';
+import ffmpeg from 'fluent-ffmpeg';
+import ffmpegPath from '@ffmpeg-installer/ffmpeg';
+import ffprobePath from '@ffprobe-installer/ffprobe';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-const platform = os.platform();
-const arch = os.arch();
-
-// 获取ffmpeg二进制文件的路径,不同平台不一样
-function setFFmpegPath() {
-  const ffPath = app.isPackaged
-    ? path.join(process.resourcesPath, 'bin')
-    : path.join(__dirname, `../../scripts/file/ff/${platform}/${arch}`);
-
-  ffmpeg.setFfprobePath(
-    path.join(ffPath, `ffprobe${platform === 'win32' ? '.exe' : ''}`),
-  );
-  ffmpeg.setFfmpegPath(
-    path.join(ffPath, `ffmpeg${platform === 'win32' ? '.exe' : ''}`),
-  );
-}
-setFFmpegPath();
-
+ffmpeg.setFfmpegPath(ffmpegPath.path.replace('app.asar', 'app.asar.unpacked'));
+ffmpeg.setFfprobePath(
+  ffprobePath.path.replace('app.asar', 'app.asar.unpacked'),
+);
 export default class FFmpeg {
   ffmpeg: ffmpeg.FfmpegCommand;
 
