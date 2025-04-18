ac11dc199034def0afa05be4eb7ea89f5eea253d
gaoSir <1933669775@qq.com>
2025-04-18 21:08:03 +0800

    feat: 快手登录问题彻底修改（最终版）

--- electron/plat/Kwai/index.ts
+++ electron/plat/Kwai/index.ts
@@ -284,6 +284,9 @@ class KwaiPub {
           partition,
         },
       });
+      mainWindow.webContents!.setUserAgent(
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0',
+      );
       mainWindow.webContents.on(
         'console-message',
         (event, level, message, line, sourceId) => {
