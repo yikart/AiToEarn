ffb4954f7f756309ce94bd34cb047999f8fdd7c6
daoyian <andaodaodao@qq.com>
2025-04-21 21:51:49 +0800

    没数据加载动画

--- src/views/statistics/statistics.tsx
+++ src/views/statistics/statistics.tsx
@@ -61,6 +61,7 @@ const Statistics = () => {
   });
 
   const [loading, setLoading] = useState(false);
+  const [loadingMore, setLoadingMore] = useState(false);
 
   useEffect(() => {
     getAccountStatistics();
@@ -304,6 +305,7 @@ const Statistics = () => {
   }
 
   async function getAccountStatistics7() {
+    setLoading(true);
     setDashboardData7([]);
     const res: any = statisticsInfo;
     const dataAll = [];
@@ -484,9 +486,9 @@ const Statistics = () => {
 
   return (
     <div className="min-h-screen page-container bg-gray-50">
-      <Spin
-        spinning={loading}
-        tip="数据加载中..."
+      <Spin 
+        spinning={loading} 
+        tip="数据加载中..." 
         size="large"
         className="min-h-screen"
       >
