8822ac7d695e97500e2ec2ca7ddfd3eec37f44af
daoyian <andaodaodao@qq.com>
2025-05-13 22:30:00 +0800

    方法注册

--- electron/main/tools/service.ts
+++ electron/main/tools/service.ts
@@ -4,7 +4,44 @@
  * @LastEditors: nevin
  * @Description: 工具箱
  */
-import { Injectable } from '../core/decorators';
+import { Inject, Injectable } from '../core/decorators';
+import { UserService } from '../user/service';
+import { AccountService } from '../account/service';
+import { InteractionService } from '../interaction/service';
 
 @Injectable()
-export class ToolsService {}
+export class ToolsService {
+  @Inject(UserService)
+  private readonly userService!: UserService;
+  @Inject(AccountService)
+  private readonly accountService!: AccountService;
+  @Inject(InteractionService)
+  private readonly interactionService!: InteractionService;
+
+  // 获取用户列表
+  async getUserList() {
+    return await this.userService.getUsers();
+  }
+
+  // 获取账户列表
+  async getAccountList(userId: string) {
+    return await this.accountService.getAccounts(userId);
+  }
+
+  // 获取自动互动列表
+  async getAutorInteractionList(account: any, worksList: any, option: any) {
+    return await this.interactionService.autorInteraction(
+        account,
+        worksList,  
+        {
+            commentContent: option.commentContent || null, 
+            platform: option.accountType, // 平台
+            likeProb: 100, // 点赞概率
+            collectProb: 100, // 收藏概率
+            commentProb: 100, // 评论概率
+            commentType: 'ai', // 评论类型
+        }
+
+    );
+  }
+}
