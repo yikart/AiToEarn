5dc20330cd078d74f4ffd41b6198ae7340fbed71
daoyian <andaodaodao@qq.com>
2025-05-27 12:30:38 +0800

    11

--- src/constant.ts
+++ src/constant.ts
@@ -1,5 +1,5 @@
 // api url
 // export const API_BASE_URL = "https://api.yikart.cn/api";
-export const API_BASE_URL = "https://platapi.aitoearn.cn/api";
-// export const API_BASE_URL = "http://localhost:3000/api";
+// export const API_BASE_URL = "https://platapi.aitoearn.cn/api";
+export const API_BASE_URL = "http://localhost:3000/api";
 
