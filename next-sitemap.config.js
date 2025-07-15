0e3443e3e7baa91b2f9410feee32bae1f7a10214
gaoSir <1933669775@qq.com>
2025-07-15 17:24:32 +0800

    chore: 测试静态站点

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,16 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.yikart.cn",
+  siteUrl: "https://apitest.aiearn.ai",
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  alternateRefs: [
+    {
+      href: "https://apitest.aiearn.ai",
+      hreflang: "en",
+    },
+    {
+      href: "https://apitest.aiearn.ai",
+      hreflang: "zh-CN",
+    },
+  ],
 };
