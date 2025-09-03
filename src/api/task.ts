0a176dc45a3a9262411a0d4b968116eed008cb91
Nevin <861796052@qq.com>
2025-09-03 20:12:43 +0800

    feat: task test page

--- src/api/task.ts
+++ src/api/task.ts
@@ -20,6 +20,46 @@ export interface TaskOpportunity {
   metadata?: Record<string, any> // 额外信息，如匹配得分等
 }
 
+export enum UserTaskStatus {
+  DOING = 'doing', // 进行中
+  PENDING = 'pending', // 待审核
+  APPROVED = 'approved', // 已通过
+  REJECTED = 'rejected', // 已拒绝
+  CANCELLED = 'cancelled', // 已取消
+  DEL = 'del', // 已删除或回退
+}
+
+interface UserTaskAutoData {
+  status: -1 | 0 | 1
+  message?: string
+}
+
+export interface UserTask {
+  _id: string;
+  id: string;
+  userId: string
+  taskId: string
+  accountId: string
+  accountType: AccountType
+  uid: string
+  status: UserTaskStatus
+  keepTime: number // 保持时间(秒)
+  submissionUrl?: string // 提交的视频、文章或截图URL
+  submissionTime?: Date // 提交时间
+  taskMaterialId?: string // 任务的素材ID
+  screenshotUrls?: string[] // 任务完成截图
+  qrCodeScanResult?: string // 二维码扫描结果
+  completionTime?: Date // 完成时间
+  rejectionReason?: string // 拒绝原因
+  metadata?: Record<string, any> // 额外信息，如审核反馈等
+  isFirstTimeSubmission: boolean // 是否首次提交，用于确定是否给予首次奖励
+  verificationNote?: string // 人工核查备注
+  reward: number // 奖励金额
+  rewardTime?: Date // 奖励发放时间
+  verifiedBy?: string // 核查人员ID
+  autoData?: UserTaskAutoData // 自动任务数据
+}
+
 // 获取待接受的任务列表
 export const apiGetTaskOpportunityList = (params: {
   page?: number;
@@ -43,7 +83,7 @@ export const apiGetUserTaskList = (params: {
   page?: number;
   pageSize?: number;
 }) => {
-  return http.get<{list: TaskOpportunity[]}>(`task/opportunity/list/${params.page}/${params.pageSize}` );
+  return http.get<{list: TaskOpportunity[]}>(`task/userTask/list/${params.page}/${params.pageSize}` );
 };
 
 /**
@@ -51,6 +91,6 @@ export const apiGetUserTaskList = (params: {
  * @param userTaskId 
  * @returns 
  */
-export const apiSubmitTask = (userTaskId: string) => {
-  return http.post<any>(`task/submit`, { userTaskId });
+export const apiSubmitTask = (userTaskId: string, submissionUrl: string) => {
+  return http.post<any>(`task/submit`, { userTaskId, submissionUrl });
 };
--- src/app/[lng]/demo/demoTask.tsx
+++ src/app/[lng]/demo/demoTask.tsx
@@ -1,25 +1,43 @@
 "use client";
-import { apiAcceptTask, apiGetTaskOpportunityList, TaskOpportunity } from "@/api/task";
+import { apiAcceptTask, apiGetTaskOpportunityList, apiGetUserTaskList, apiSubmitTask, TaskOpportunity } from "@/api/task";
 import { useState } from "react";
 
 export const DemoTask = () => {
   const [result, setResult] = useState<TaskOpportunity[]>([]);
+  const [userTaskList, setUserTaskList] = useState<any[]>([]);
 
-  
+
 
   async function getTaskOpportunityList() {
     const res = await apiGetTaskOpportunityList({ page: 1, pageSize: 20 });
     console.log("------ createMaterialGroup ---- ", res);
-    if(!res) return;
+    if (!res) return;
     setResult(res.data.list);
   }
 
-  async function acceptTask(opportunityId:string) {
+  async function acceptTask(opportunityId: string) {
     console.log(`----`, opportunityId);
-    
+
     const res = await apiAcceptTask(opportunityId)
     console.log('----- res', res);
   }
+
+
+  async function getUserTaskList() {
+    const res = await apiGetUserTaskList({ page: 1, pageSize: 20 });
+    console.log("------ getUserTaskList ---- ", res);
+    if (!res) return;
+    setUserTaskList(res.data.list);
+  }
+
+
+  async function submitTask(userTaskId: string) {
+    console.log(`----`, userTaskId);
+
+    const res = await apiSubmitTask(userTaskId, 'https://www.baidu.com')
+    console.log('----- res', res);
+  }
+
 
   return (
     <div>
@@ -27,7 +45,7 @@ export const DemoTask = () => {
       <div>
         <button onClick={getTaskOpportunityList}>获取发放给我的任务列表</button>
 
-{/* 待接受任务列表 */}
+        {/* 待接受任务列表 */}
         <div>
 
           {result.map((item) => (
@@ -36,21 +54,23 @@ export const DemoTask = () => {
               <div>{item.accountId}</div>
               <div>{item.accountType}</div>
               <div>{item.status}</div>
-              <button onClick={()=>acceptTask(item._id)}>接受任务</button>
+              <button onClick={() => acceptTask(item._id)}>接受任务</button>
             </div>
           ))}
         </div>
 
-{/* 已接受任务列表 */}
+
+        <button onClick={getUserTaskList}>获取我接受的任务列表</button>
+        {/* 已接受任务列表 */}
         <div>
 
-          {result.map((item) => (
+          {userTaskList.map((item) => (
             <div key={item._id}>
               <div>{item._id}</div>
               <div>{item.accountId}</div>
               <div>{item.accountType}</div>
               <div>{item.status}</div>
-              <button onClick={()=>acceptTask(item._id)}>接受任务</button>
+              <button onClick={() => submitTask(item._id)}>提交任务</button>
             </div>
           ))}
         </div>
