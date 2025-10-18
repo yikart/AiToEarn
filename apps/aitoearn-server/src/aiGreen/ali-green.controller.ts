fb14d19e0ecf866012d0aa9256e5cbafaa389103
Nevin <861796052@qq.com>
2025-10-18 16:15:36 +0800

    feat: 阿里内容检测迁入

---
+++ apps/aitoearn-server/src/aiGreen/ali-green.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Post } from '@nestjs/common'
+import { Payload } from '@nestjs/microservices'
+import { GetToken } from '../auth/auth.guard'
+import { TokenInfo } from '../auth/interfaces/auth.interfaces'
+import { AliGreenService } from './ali-green.service'
+import { ImageBodyDto, TextBodyDto, VideoBodyDto, VideoResultBodyDto } from './dto/ali-green.dto'
+
+@Controller('aliGreen')
+export class AliGreenController {
+  constructor(
+    private readonly aliGreenService: AliGreenService,
+  ) {}
+
+  // 文本审核  限制在2000字以内
+  @Post('textGreen/')
+  textGreen(@GetToken() token: TokenInfo, @Payload() data: TextBodyDto) {
+    return this.aliGreenService.textGreen(data, token)
+  }
+
+  // 图片审核  限制频率 qps为100  每张图片大小限制为20M以内
+  @Post('imgGreen/')
+  imgGreen(@GetToken() token: TokenInfo, @Payload() data: ImageBodyDto) {
+    return this.aliGreenService.imgGreen(data, token)
+  }
+
+  // 视频审核  限制视频大小 为500M以内  格式为mp4 flv
+  @Post('videoGreen/')
+  videoGreen(@GetToken() token: TokenInfo, @Payload() data: VideoBodyDto) {
+    return this.aliGreenService.videoGreen(data, token)
+  }
+
+  @Post('getVideoResult/')
+  getVideoResult(@GetToken() token: TokenInfo, @Payload() data: VideoResultBodyDto) {
+    return this.aliGreenService.getVideoResult(data, token)
+  }
+}
