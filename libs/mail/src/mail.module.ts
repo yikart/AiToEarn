2cba9d4cc91ae11c83d4272f28f776be4ace3e21
Nevin <861796052@qq.com>
2025-10-18 16:40:50 +0800

    style: 1

--- libs/mail/src/mail.module.ts
+++ libs/mail/src/mail.module.ts
@@ -1,8 +1,9 @@
 import { MailerModule } from '@nestjs-modules/mailer'
-import { DynamicModule, Module } from '@nestjs/common'
+import { DynamicModule, Global, Module } from '@nestjs/common'
 import { MailConfig } from './mail.config'
 import { MailService } from './mail.service'
 
+@Global()
 @Module({})
 export class MailModule {
   static forRoot(config: MailConfig): DynamicModule {
