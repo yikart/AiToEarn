b1c65ae90055d1c4bb8b756a75603defb1100e6a
Nevin <861796052@qq.com>
2025-10-18 23:18:10 +0800

    feat: 互动模块迁入

---
+++ apps/aitoearn-server/src/channel/interact/interactionRecord.service.ts
@@ -0,0 +1,74 @@
+import { HttpService } from '@nestjs/axios'
+import { Injectable } from '@nestjs/common'
+import { TableDto } from '@yikart/common'
+import { AccountType } from '@yikart/mongodb'
+import { InteractionRecord } from './common'
+
+@Injectable()
+export class InteractionRecordService {
+  constructor(
+    private readonly httpService: HttpService,
+  ) { }
+
+  async add(data: {
+    userId: string
+    accountId: string
+    type: AccountType
+    worksId: string
+    worksTitle?: string
+    worksCover?: string
+    worksContent?: string
+    commentContent?: string
+    commentRemark?: string
+    commentTime?: string
+    likeTime?: string
+    collectTime?: string
+  }) {
+    const res = await this.httpService.axiosRef.post<boolean>(
+      'http://127.0.0.1:3000/api/channel/interactionRecord/add',
+      data,
+    )
+    return res.data
+  }
+
+  /**
+   * 获取作品评论列表
+   * @returns
+   */
+  async list(userId: string, filters: {
+    accountId?: string
+    type?: AccountType
+    worksId?: string
+    time?: [Date, Date]
+  }, page: TableDto) {
+    const res = await this.httpService.axiosRef.post<{
+      list: InteractionRecord[]
+      total: number
+    }>(
+      'http://127.0.0.1:3000/api/channel/interactionRecord/list',
+      {
+        filters: {
+          userId,
+          ...filters,
+        },
+        page,
+      },
+    )
+    return res.data
+  }
+
+  /**
+   * 删除
+   * @param id
+   * @returns
+   */
+  async del(id: string) {
+    const res = await this.httpService.axiosRef.post<boolean>(
+      'http://127.0.0.1:3000/api/channel/interactionRecord/del',
+      {
+        id,
+      },
+    )
+    return res.data
+  }
+}
