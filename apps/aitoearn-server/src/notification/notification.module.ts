fc33dc8e5e7fc1f7fdbe6a066d48004b1f1976d9
Nevin <861796052@qq.com>
2025-10-19 23:40:10 +0800

    feat: 应用配置迁入

--- apps/aitoearn-server/src/notification/notification.module.ts
+++ apps/aitoearn-server/src/notification/notification.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common'
+import { OneSignalModule } from '@yikart/one-signal'
+import { config } from '../config'
 import { NotificationController } from './notification.controller'
 import { NotificationService } from './notification.service'
 
 @Module({
+  imports: [
+    OneSignalModule.register(config.oneSignal),
+  ],
   controllers: [NotificationController],
   providers: [NotificationService],
   exports: [NotificationService],
