e3ff75a50f76712ce9294cec636226d01b20172f
Nevin <861796052@qq.com>
2025-10-19 23:40:10 +0800

    feat: 应用配置迁入

--- apps/aitoearn-server/src/app-configs/app-config.module.ts
+++ apps/aitoearn-server/src/app-configs/app-config.module.ts
@@ -1,11 +1,8 @@
-import { Global, Module } from '@nestjs/common'
+import { Module } from '@nestjs/common'
 import { AppConfigController } from './app-config.controller'
 import { AppConfigService } from './app-config.service'
 
-@Global()
 @Module({
-  imports: [
-  ],
   providers: [AppConfigService],
   controllers: [AppConfigController],
 })
