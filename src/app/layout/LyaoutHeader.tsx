d2ff2862ee1a416b3acb35cc95282373f810ea15
gaoSir <1933669775@qq.com>
2025-10-22 18:20:01 +0800

    perf: Optimize the menu in the upper right corner

--- src/app/layout/LyaoutHeader.tsx
+++ src/app/layout/LyaoutHeader.tsx
@@ -1,16 +1,24 @@
 "use client";
 
-import { ForwardedRef, forwardRef, memo, useRef, useState } from "react";
+import {
+  ForwardedRef,
+  forwardRef,
+  memo,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import styles from "./styles/lyaoutHeader.module.scss";
 import { useUserStore } from "@/store/user";
 import Link from "next/link";
 import Image from "next/image";
 import LayoutNav from "@/app/layout/layoutNav";
 import { NoSSR } from "@kwooshung/react-no-ssr";
-import { Button, Dropdown, MenuProps, Badge } from "antd";
+import { Button, Badge, Popover } from "antd";
 import {
+  ArrowLeftOutlined,
   BellOutlined,
-  CaretDownOutlined,
+  BookOutlined,
   CrownOutlined,
 } from "@ant-design/icons";
 import { useRouter } from "next/navigation";
@@ -22,6 +30,14 @@ import SignInCalendar from "@/components/SignInCalendar";
 import VipContentModal from "@/components/modals/VipContentModal";
 import PointsDetailModal from "@/components/modals/PointsDetailModal";
 import LanguageSwitcher from "@/components/common/LanguageSwitcher";
+import {
+  UserOutlined,
+  FileTextOutlined,
+  WalletOutlined,
+  GithubOutlined,
+  LogoutOutlined,
+  QuestionCircleOutlined,
+} from "@ant-design/icons";
 
 import logo from "@/assets/images/logo.png";
 
@@ -33,96 +49,130 @@ function UserInfo() {
   const userInfo = useUserStore((state) => state.userInfo)!;
   const router = useRouter();
   const { t } = useTransClient("common");
-  const { t: tVip } = useTransClient("vip");
-  const lng = useGetClientLng();
 
-  const items: MenuProps["items"] = [
-    {
-      key: "1",
-      label: (
-        <div
-          onClick={() => {
-            router.push(`/${lng}/profile`);
-          }}
-        >
-          {t("profile")}
-        </div>
-      ),
-    },
-    {
-      key: "2",
-      label: (
-        <div
-          onClick={() => {
-            router.push(`/${lng}/income`);
-          }}
-        >
-          {t("header.income" as any)}
-        </div>
-      ),
-    },
-    {
-      key: "3",
-      label: (
-        <div
-          onClick={() => {
-            router.push(`/${lng}/wallet`);
-          }}
-        >
-          {t("header.wallet" as any)}
-        </div>
-      ),
-    },
-    // {
-    //   key: "4",
-    //   label: (
-    //     <div
-    //       onClick={() => {
-    //         router.push(`/${lng}/notification`);
-    //       }}
-    //     >
-    //       {t("header.messages")}
-    //     </div>
-    //   ),
-    // },
+  const menuItems = useMemo(() => {
+    return [
+      {
+        href: `/profile`,
+        label: t("profile"),
+        icon: <UserOutlined />,
+        border: true,
+      },
+      {
+        href: `/income`,
+        label: t("header.income"),
+        icon: <FileTextOutlined />,
+      },
+      {
+        href: `/wallet`,
+        label: t("header.wallet"),
+        icon: <WalletOutlined />,
+        border: true,
+      },
+      {
+        href: `https://docs.aitoearn.ai/`,
+        label: t("docs"),
+        icon: <BookOutlined />,
+      },
+      {
+        label: t("support"),
+        icon: <QuestionCircleOutlined />,
+        href: "https://github.com/yikart/AiToEarn/issues",
+      },
+      {
+        label: "Github",
+        icon: <GithubOutlined />,
+        href: `https://github.com/yikart/AiToEarn`,
+        subtitle: (
+          <img src="https://img.shields.io/github/stars/yikart/AiToEarn.svg" />
+        ),
+      },
+      {
+        onClick: () => {
+          useUserStore.getState().logout();
+          router.push(`/`);
+        },
+        label: t("logout"),
+        icon: <LogoutOutlined />,
+      },
+    ];
+  }, [router, t]);
 
-    {
-      key: "7",
-      label: (
-        <div
-          onClick={() => {
-            useUserStore.getState().logout();
-            router.push(`/${lng}/`);
-          }}
-        >
-          {t("logout")}
-        </div>
-      ),
-    },
-  ];
+  const avatar = (
+    <Image
+      className={styles["layoutHeader-userinfo-avatar"]}
+      src={userInfo?.avatar || logo}
+      alt={t("profile")}
+      width={35}
+      height={35}
+      style={{
+        borderRadius: "50%",
+        backgroundColor: "#e9d5ff",
+        padding: "3px",
+      }}
+    />
+  );
 
   return (
     <div className={styles["layoutHeader-userinfo__wrapper"]}>
-      <Dropdown menu={{ items }} arrow placement="bottom">
+      <Popover
+        placement="bottomLeft"
+        arrow={false}
+        styles={{ body: { padding: "0" } }}
+        content={
+          <div className={styles.layoutHeaderMenu}>
+            <div className="layoutHeaderMenu-head">
+              <div className="layoutHeaderMenu-head-info">
+                <span className="layoutHeaderMenu-head-info-name">
+                  {userInfo.name}
+                </span>
+                <span className="layoutHeaderMenu-head-info-mail">
+                  {userInfo.mail}
+                </span>
+              </div>
+              <div className="layoutHeaderMenu-head-avatar">{avatar}</div>
+            </div>
+            <div className="layoutHeaderMenu-content">
+              {menuItems.map((v) => {
+                return (
+                  <a
+                    className={`layoutHeaderMenu-item ${v.border ? "layoutHeaderMenu-item--border" : ""}`}
+                    href={v.href}
+                    key={v.label}
+                    title={v.label}
+                    target={v.href?.includes("https://") ? "_blank" : "_self"}
+                    onClick={() => v.onClick}
+                  >
+                    <p className="layoutHeaderMenu-item-label">
+                      {v.icon}
+                      {v.label}
+                    </p>
+                    <div className="layoutHeaderMenu-item-subtitle">
+                      {v.subtitle
+                        ? v.subtitle
+                        : v.href && (
+                            <ArrowLeftOutlined
+                              style={{
+                                transform: "rotate(140deg)",
+                                fontSize: "12px",
+                              }}
+                            />
+                          )}
+                    </div>
+                  </a>
+                );
+              })}
+            </div>
+          </div>
+        }
+      >
         <div className={styles["layoutHeader-userinfo"]}>
-          <Image
-            className={styles["layoutHeader-userinfo-avatar"]}
-            src={userInfo?.avatar || logo}
-            alt={t("profile")}
-            width={35}
-            height={35}
-            style={{
-              borderRadius: "50%",
-              backgroundColor: "#e9d5ff",
-              padding: "3px",
-            }}
-          />
+          {avatar}
           <div className={styles["layoutHeader-userinfo-name"]}>
             {userInfo?.name || t("unknownUser")}
           </div>
-          <CaretDownOutlined />
         </div>
-      </Dropdown>
+      </Popover>
     </div>
   );
 }
