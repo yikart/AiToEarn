1acb9e5ea754896d53b0532b70855d4c03141718
bulaienaofutuojiesenzhang <767653317@qq.com>
2025-10-23 14:44:20 +0800

    router direct access healthz

--- src/middleware.ts
+++ src/middleware.ts
@@ -27,6 +27,7 @@ export function middleware(req: NextRequest) {
       "/sitemap-4.xml",
       "/sitemap",
       "/chat",
+      "/healthz",
     ].find((v) => req.nextUrl.pathname.includes(v!))
   ) {
     return NextResponse.next();
