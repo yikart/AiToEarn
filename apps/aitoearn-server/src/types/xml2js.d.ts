3d6510ba9b219b8e78febca1d925513017737718
wheat <wheatsliu@gmail.com>
2025-10-24 18:32:59 +0800

    fix(format): eslint autofix
    Signed-off-by: wheat <wheatsliu@gmail.com>

--- apps/aitoearn-server/src/types/xml2js.d.ts
+++ apps/aitoearn-server/src/types/xml2js.d.ts
@@ -2,11 +2,11 @@ declare module 'xml2js' {
   export function parseString(
     str: string,
     options: { explicitArray: boolean },
-    callback: (err: any, result: any) => void
+    callback: (err: any, result: any) => void,
   ): void
 
   export function parseString(
     str: string,
-    callback: (err: any, result: any) => void
+    callback: (err: any, result: any) => void,
   ): void
 }
