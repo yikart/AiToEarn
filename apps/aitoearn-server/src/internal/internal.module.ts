7ddfde38de3626fb4c70067bc5a4581d1ebde7f5
Nevin <861796052@qq.com>
2025-10-24 19:26:15 +0800

    fix: 1

--- apps/aitoearn-server/src/internal/internal.module.ts
+++ apps/aitoearn-server/src/internal/internal.module.ts
@@ -1,5 +1,5 @@
 import { Global, Module } from '@nestjs/common'
-import { AccountModule } from '../statistics/accountData/accountData.module'
+import { AccountModule } from '../account/account.module'
 import { InternalService } from './internal.service'
 
 @Global()
