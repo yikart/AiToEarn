a4d93738e0b03b6d39f95c9abf9d5f05b599f6a0
Nevin <861796052@qq.com>
2025-10-24 08:39:35 +0800

    fix: 1

--- apps/aitoearn-server/src/transports/task/api/taskApi.module.ts
+++ apps/aitoearn-server/src/transports/task/api/taskApi.module.ts
@@ -1,6 +1,5 @@
 import { HttpModule } from '@nestjs/axios'
 import { Global, Module } from '@nestjs/common'
-import { TaskMaterialNatsApi } from './material.natsApi'
 import { TaskPortraitNatsApi } from './portrait.natsApi'
 import { TaskNatsApi } from './task.natsApi'
 import { UserTaskNatsApi } from './user-task.natsApi'
@@ -8,7 +7,7 @@ import { UserTaskNatsApi } from './user-task.natsApi'
 @Global()
 @Module({
   imports: [HttpModule],
-  providers: [UserTaskNatsApi, TaskNatsApi, TaskPortraitNatsApi, TaskMaterialNatsApi],
-  exports: [UserTaskNatsApi, TaskNatsApi, TaskPortraitNatsApi, TaskMaterialNatsApi],
+  providers: [UserTaskNatsApi, TaskNatsApi, TaskPortraitNatsApi],
+  exports: [UserTaskNatsApi, TaskNatsApi, TaskPortraitNatsApi],
 })
 export class TaskApiModule {}
--- apps/aitoearn-server/src/transports/task/taskApi.module.ts
+++ apps/aitoearn-server/src/transports/task/taskApi.module.ts
@@ -1,6 +1,5 @@
 import { HttpModule } from '@nestjs/axios'
 import { Global, Module } from '@nestjs/common'
-import { TaskMaterialNatsApi } from './api/material.natsApi'
 import { TaskPortraitNatsApi } from './api/portrait.natsApi'
 import { TaskNatsApi } from './api/task.natsApi'
 import { UserTaskNatsApi } from './api/user-task.natsApi'
@@ -8,7 +7,7 @@ import { UserTaskNatsApi } from './api/user-task.natsApi'
 @Global()
 @Module({
   imports: [HttpModule],
-  providers: [UserTaskNatsApi, TaskNatsApi, TaskPortraitNatsApi, TaskMaterialNatsApi],
-  exports: [UserTaskNatsApi, TaskNatsApi, TaskPortraitNatsApi, TaskMaterialNatsApi],
+  providers: [UserTaskNatsApi, TaskNatsApi, TaskPortraitNatsApi],
+  exports: [UserTaskNatsApi, TaskNatsApi, TaskPortraitNatsApi],
 })
 export class TaskApiModule { }
