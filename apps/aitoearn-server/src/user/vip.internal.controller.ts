0c1180fe1b4591f8055811cabfa163836ee07319
Nevin <861796052@qq.com>
2025-10-28 19:23:00 +0800

    WIP changes

--- apps/aitoearn-server/src/user/vip.internal.controller.ts
+++ apps/aitoearn-server/src/user/vip.internal.controller.ts
@@ -1,10 +1,8 @@
 import { Body, Controller, Post } from '@nestjs/common'
-import { Internal } from '@yikart/aitoearn-auth'
 import { VipStatus } from '@yikart/mongodb'
 import { VipService } from './vip.service'
 
 @Controller()
-@Internal()
 export class VipInternalController {
   constructor(
     private readonly vipService: VipService,
