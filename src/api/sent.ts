11efa26833e938477e517d4f71b0b7c610fadbe9
bulaienaofutuojiesenzhang <767653317@qq.com>
2025-10-28 16:15:10 +0800

    all

--- src/api/sent.ts
+++ src/api/sent.ts
@@ -4,7 +4,7 @@ import { SentPostsParams, SentPostsResponse } from "./types/sent.types";
 // 获取已发布帖子列表
 export const getSentPosts = (params: SentPostsParams) => {
   return request<SentPostsResponse>({
-    url: "channel/engagement/posts",
+    url: "statistics/posts/list",
     method: "POST",
     data: params,
   });
