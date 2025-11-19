51b98881a4d04ae1edcfd860e102cd69d852d5ce
Nevin <861796052@qq.com>
2025-11-19 18:28:28 +0800

    refactor(listmonk): remove dependency on @yikart/common and clean up base service code

--- project/backend/libs/listmonk/src/api/base.service.ts
+++ project/backend/libs/listmonk/src/api/base.service.ts
@@ -1,5 +1,4 @@
-import { Inject, Injectable, Logger } from '@nestjs/common'
-import { COMMON_PROPAGATION_HEADERS, propagationContext } from '@yikart/common'
+import { Inject, Injectable } from '@nestjs/common'
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
 import { ListmonkConfig } from '../interfaces'
 
@@ -10,7 +9,6 @@ interface ListmonkResponse<T> {
 @Injectable()
 export class BaseService {
   protected readonly httpClient: AxiosInstance
-  private readonly logger = new Logger(BaseService.name)
   constructor(
     @Inject('LISTMONK_CONFIG') private readonly config: ListmonkConfig,
   ) {
@@ -24,20 +22,6 @@ export class BaseService {
       headers: {
         'Content-Type': 'application/json',
       },
-    })
-
-    this.httpClient.interceptors.request.use((request) => {
-      const store = propagationContext.getStore()
-      if (store == null)
-        return request
-      COMMON_PROPAGATION_HEADERS
-        .forEach((key) => {
-          const value = store.headers[key]
-          if (value) {
-            request.headers.set(key, value)
-          }
-        })
-      return request
     })
   }
 
