361ecae81c3ac5d384c67639c188294103be5bf9
Nevin <861796052@qq.com>
2025-11-27 21:34:54 +0800

    refactor(notification): reorganize imports and update notification enums for consistency

--- project/backend/apps/aitoearn-server/src/notification/notification.service.ts
+++ project/backend/apps/aitoearn-server/src/notification/notification.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleInit } from '@nestjs/common'
-import { AppException, ResponseCode, UserType } from '@yikart/common'
-import { NotificationRepository, NotificationStatus, NotificationType } from '@yikart/mongodb'
+import { AppException, NotificationType, ResponseCode, UserType } from '@yikart/common'
+import { NotificationRepository, NotificationStatus } from '@yikart/mongodb'
 import {
   BatchDeleteDto,
   MarkAsReadDto,
---
+++ project/backend/libs/common/src/enums/notification.enum.ts
@@ -0,0 +1,13 @@
+// 通知状态枚举
+export enum NotificationStatus {
+  Unread = 'unread',
+  Read = 'read',
+}
+
+// 通知类型枚举
+export enum NotificationType {
+  TaskReminder = 'task_reminder',
+  UserWithdraw = 'user_withdraw',
+  TaskPunish = 'task_punish',
+  TaskDemandAdd = 'task_demand_add',
+}
